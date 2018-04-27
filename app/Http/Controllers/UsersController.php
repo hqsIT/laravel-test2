82e8f5fcab51bf812d434c046b041e14da021f81
klinson <klinson@163.com>
2018-04-27 17:41:23 +0800

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handles\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -18,9 +19,25 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    /**
+     * @param UserRequest $request 使用专用请求器, 内置自动验证权限和验证数据
+     * @param ImageUploadHandler $uploader 使用工具类进行图片上传
+     * @param User $user
+     * @author klinson <klinson@163.com>
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+        //$request->avatar 可以直接拿出文件信息
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -30,6 +30,8 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            //rules() 方法中新增了图片比例验证规则 dimensions ，仅允许上传宽和高都大于 200px 的图片；
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=200,min_height=200',
         ];
     }
 
@@ -43,6 +45,8 @@ class UserRequest extends FormRequest
     {
 //        return parent::messages();
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 200px 以上',
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持英文、数字、横杆和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
