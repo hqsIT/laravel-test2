9511b2f68bb5d26692e8720086d97dfb0fa64438
klinson <klinson@163.com>
2018-04-27 16:42:27 +0800

    Basic user profile editing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
     {
         // MySql支持的utf8编码最大字符长度为3字节，如果遇到4字节的宽字符就会出现插入异常,　需要调整字符串长度解决
         Schema::defaultStringLength(191);
+        // Carbon 是 PHP DateTime 的一个简单扩展, 调整汉语
+        Carbon::setLocale('zh');
     }
 
     /**
