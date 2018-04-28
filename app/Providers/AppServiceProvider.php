edf3402dc125fd8cb1c147042c6c740b05418bc5
klinson <klinson@163.com>
2018-04-28 11:31:20 +0800

    Generated topics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
         // MySql支持的utf8编码最大字符长度为3字节，如果遇到4字节的宽字符就会出现插入异常,　需要调整字符串长度解决
         Schema::defaultStringLength(191);
         // Carbon 是 PHP DateTime 的一个简单扩展, 调整汉语
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/test', 'TestController@index');
 Auth::routes();
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
