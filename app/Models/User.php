77792a8bbb6dd81e501916b6dfcf55753267fa33
klinson <klinson@163.com>
2018-05-07 15:56:43 +0800

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'phone', 'password', 'introduction', 'avatar'
+        'name', 'email', 'phone', 'password', 'introduction', 'avatar', 'weixin_openid', 'weixin_unionid'
     ];
 
     /**
