c0361389c53e45e6c5eab144554ca6e2f7c9ca1b
klinson <klinson@163.com>
2018-05-09 16:51:27 +0800

    replies index

--- routes/api.php
+++ routes/api.php
@@ -56,12 +56,21 @@ $api->version('v1', [
         // 游客可以访问的接口
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+        // 某个用户发布的话题
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+        // 话题详情
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
         // 需要 token 验证的接口
         // DingoApi 为我们准备好了 api.auth 这个中间件，用来区分哪些接口需要验证 token，哪些不需要
